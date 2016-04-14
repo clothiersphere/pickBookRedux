b22a6d9855aa65b27bbe82e3d0fb91512f3921da
Spacemixup <xchrislee@gmail.com>
2016-04-14 13:28:26 -0700

    Action Creator function created.

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 	renderList() {
@@ -10,8 +12,7 @@ class BookList extends Component {
 		});
 	}
 
-
-	render () {
+	render() {
 		return (
 			<ul className="list-group col-sm-4">
 				{this.renderList()}
@@ -30,4 +31,12 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+	//Whenever selectBook is called, the result should be passed to all of our reducers
+	//now we can call this.props.selectBook - which will call action creator
+	return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+//promote BookList from component into container. Make selectBook available as prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
