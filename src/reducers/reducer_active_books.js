93d81accf2fd210f50b14982280a051c0ae11cac
Spacemixup <xchrislee@gmail.com>
2016-04-14 14:21:47 -0700

    Book picker finished

--- src/reducers/reducer_active_books.js
+++ src/reducers/reducer_active_books.js
@@ -1,6 +1,6 @@
 //State argument is not application state, only the state this reducer is responsible for
 //state = null es6 - when the argument is called without state
-export default function (state = null, action) {
+export default function(state = null, action) {
 	switch(action.type) {
 	case 'BOOK_SELECTED': 
 		return action.payload;
